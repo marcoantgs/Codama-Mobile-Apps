ad5536d68787bfae78ca0ac23419f8cdf12787b3
Lucas de Sena Bastos <lucasdesena@ucl.br>
2021-06-16 11:28:28 -0300

    Pequenas alterações na estrutura da lista de comodos e adição do método para troca de tela ao clicar no comodo

--- constructo/lib/components/comodo_lista.dart
+++ constructo/lib/components/comodo_lista.dart
@@ -1,3 +1,4 @@
+import 'package:constructo/components/tela_comodo.dart';
 import 'package:constructo/models/comodo.dart';
 import 'package:flutter/material.dart';
 
@@ -13,61 +14,71 @@ class ComodoLista extends StatelessWidget {
         child: ListView.builder(
           itemCount: comodos.length,
           itemBuilder: (ctx, index) {
+            bool teste = false;
             final co = comodos[index];
-            return Card(
-                child: Row(
-              children: <Widget>[
-                Container(
-                  color: Colors.black,
-                  width: 80,
-                  height: 60,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 20, left: 25),
-                    child: Text(
-                      co.valorTotal.toString(),
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15,
-                      ),
-                    ),
-                  ),
-                ),
-                Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 8),
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => TelaComodo(comodos[index])));
+              },
+              //Pensar em como fazer o update e o delete por esse metodo
+              onLongPress: () {},
+              child: Card(
+                child: Row(children: <Widget>[
+                  Container(
+                    color: Colors.black,
+                    width: 80,
+                    height: 60,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 20, left: 25),
                       child: Text(
-                        co.titulo,
+                        co.valorTotal.toString(),
                         style: TextStyle(
-                          color: Colors.black,
+                          color: Colors.white,
                           fontSize: 15,
                         ),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 25, bottom: 12),
-                      child: Text(
-                        co.descricao,
-                        style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 10,
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 8),
+                        child: Text(
+                          co.titulo,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                          ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 25, bottom: 12),
-                      child: Text(
-                        co.tipoComodo,
-                        style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 10,
+                      Padding(
+                        padding: const EdgeInsets.only(left: 25, bottom: 12),
+                        child: Text(
+                          co.descricao,
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 10,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                )
-              ],
-            ));
+                      Padding(
+                        padding: const EdgeInsets.only(left: 25, bottom: 12),
+                        child: Text(
+                          co.tipoComodo,
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 10,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ]),
+              ),
+            );
           },
         ),
       ),
