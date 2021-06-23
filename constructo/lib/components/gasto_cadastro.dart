76abd06d4a43eeef661f2ef051e7ff8184259ca3
Acley Filho <50428838+acleyfilho@users.noreply.github.com>
2021-06-22 23:24:29 -0300

    Alteração do layout do cadastro de gastos

--- constructo/lib/components/gasto_cadastro.dart
+++ constructo/lib/components/gasto_cadastro.dart
@@ -1,3 +1,4 @@
+import 'package:constructo/components/sobre.dart';
 import 'package:constructo/models/comodo.dart';
 import 'package:constructo/models/gasto.dart';
 import 'package:constructo/utils/OperacoesGasto.dart';
@@ -16,6 +17,19 @@ class _CadastroGastoState extends State<CadastroGasto> {
 
   final tituloController = TextEditingController();
   final valorController = TextEditingController();
+
+  _trocaDeTela(int index) {
+    if (index == 0) {
+      setState(() {
+        Navigator.popAndPushNamed(context, '/home');
+      });
+    } else if (index == 2) {
+      setState(() {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => Sobre()));
+      });
+    }
+  }
 
   _btCadastrar() {
     //Pegando os valores
@@ -45,24 +59,100 @@ class _CadastroGastoState extends State<CadastroGasto> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
+    return Scaffold(
+      body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            TextField(
-              controller: tituloController,
-              decoration: InputDecoration(labelText: 'Título'),
+            Container(
+              color: Color.fromARGB(255, 72, 34, 16),
+              height: 130,
+              width: double.infinity,
+              child: Center(
+                child: Text(
+                  "Cadastrar gasto",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                  ),
+                ),
+              ),
             ),
-            TextField(
-                controller: valorController,
-                decoration: InputDecoration(labelText: 'valor')),
-            FlatButton(child: Text('Novo Gasto'), onPressed: _btCadastrar),
-            FlatButton(child: Text('Cancelar'), onPressed: _btCancelar)
+            Card(
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      controller: tituloController,
+                      decoration: InputDecoration(labelText: 'Título'),
+                    ),
+                    TextField(
+                      controller: valorController,
+                      decoration: InputDecoration(labelText: 'valor')),
+                    Container(
+                      padding: const EdgeInsets.all(10.0),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(5.0),
+                      margin: const EdgeInsets.only(top: 20.0),
+                      height: 45,
+                      width: 400,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Color.fromARGB(255, 72, 34, 16), // background
+                          onPrimary: Colors.white, // foreground
+                        ),
+                        child: Text('Novo Gasto'),
+                        onPressed: _btCadastrar),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(5.0),
+                      height: 45,
+                      width: 400,
+                      child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.brown[50], // background
+                          onPrimary: Colors.black, // foreground
+                        ),
+                      child: Text('Cancelar'), 
+                      onPressed: _btCancelar)
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Color.fromARGB(255, 72, 34, 16),
+        iconSize: 40,
+        selectedFontSize: 15,
+        unselectedFontSize: 15,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_box_outlined), label: 'Cômodo'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.info_outline), label: 'Sobre'),
+        ],
+        onTap: (index) {
+          _trocaDeTela(index);
+        },
+      ),
     );
   }
+
+
+
+
+
+
+
+
 }
